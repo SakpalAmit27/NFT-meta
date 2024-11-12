7310a0e2806dacfda197730a34de5fa4e080e5d9
Sakpal Amit <amitsakpal744@gmail.com>
2024-11-12 19:07:42 +0530

    completed the deployment script

--- ignition/modules/Token.js
+++ ignition/modules/Token.js
@@ -2,5 +2,12 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 const DeployModule = buildModule('TokenModule',(m) => {
-  
+  // creating instance form our smart contract // 
+
+  const marketPLace = m.contract("NFTSTORE"); 
+  return marketPLace;
+
+
 })
+
+module.exports = DeployModule;
