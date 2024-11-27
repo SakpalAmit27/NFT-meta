8efd1048386586f53d897189fc4adab0fef756bc
Sakpal Amit <amitsakpal744@gmail.com>
2024-11-27 13:45:41 +0530

    writing functions to connect to pinata ipfs

---
+++ nftmeta/src/app/pinata.js
@@ -0,0 +1,59 @@
+"use server"
+
+import { headers } from 'next/headers';
+
+const axios = require('axios')
+
+const jwt = process.env.JWT;
+
+
+export const uploadJSONToIPFS = async(JSONBody) =>{ 
+
+    const url = 'https://api.pinata.cloud/pinning/pinJSONToIPFS';
+
+    try{
+
+        const res = await axios.post(url,JSONBody,{
+            headers:{
+                Authorization:`Bearer ${jwt}`
+            }
+        })
+
+        return{
+            success:true,
+            pinataURL:"https://gateway.pinata.cloud/ipfs/" + res.data.IpfsHash,
+
+        }
+        
+    }catch(error){
+        console.log(error)
+
+        return{
+            success:false,
+            message:error.message
+        }
+    }
+
+
+}
+
+export const uploadFileToIPFS = async(data) => {
+    const pinanataMetadata = JSON.stringify({
+
+        name:data.get('file').name,
+
+
+        
+    })
+    data.append("pinataMetadata",pinanataMetadata);
+
+    const pinataOptions = JSON.stringify({
+        cidVersion:0,
+
+    })
+    data.append("pinataoptions",pinataOptions); 
+
+    try{
+        const res = await axios.post('https://api.pinata.cloud/pinning/pinJSONToIPFS')
+    }
+}
